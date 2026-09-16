47830999271f8409661af326c4629f1daad1b798
Anna <anna@hypership.tech>
2026-09-16 12:56:26 +0100

    Add all HTML elements

---
+++ calculator.js
@@ -0,0 +1,36 @@
+// function add(a, b) {
+//   return a + b;
+// }
+
+// function subtract(a, b) {
+//   return a - b;
+// }
+
+// function multiply(a, b) {
+//   return a * b;
+// }
+
+// function divide(a, b) {
+//   return a / b;
+// }
+
+// let firstNumber = a;
+// let operator = b;
+// let secondNumber = c;
+
+// function operate(firstNumber, operator, secondNumber) {
+//   add(firstNumber, secondNumber);
+//   subtract(firstNumber, secondNumber);
+//   multiply(firstNumber, secondNumber);
+//   divide(firstNumber, secondNumber);
+// }
+
+// create 16 buttons
+// const buttons_container = document.querySelector(".buttons-container");
+
+// for (let index = 0; index < 15; index++) {
+//   const button = document.createElement("button");
+//   button.classList.add("button");
+//   buttons_container.appendChild(button);
+// }
+
